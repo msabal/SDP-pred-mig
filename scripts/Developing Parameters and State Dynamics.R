4a21909d93841c469386957b9fe4ef25721dc7b0
Megan Sabal <megan.sabal@gmail.com>
2021-06-23 17:50:00 -0700

    Eq.s 3 and 4 progress

--- scripts/Developing Parameters and State Dynamics.R
+++ scripts/Developing Parameters and State Dynamics.R
@@ -41,7 +41,7 @@ plot(Weight_ ~ Length, data=samdata) #ALL species (including other than salmon) 
   # Both Coleman and Mill Creek fish are from/released near the Upper Sacramento River (similar to how our model is set up)
 
 
-# FIRST, tried more slamon groups, but then data distribution is all wonky.
+# FIRST, tried more salmon groups, but then data distribution is all wonky.
   samdata$PopGroup <- substr(samdata$StudyID, 1, 7) #Make new column PopGroup with only first 7 characters from StudyID, I picked 7 so that I get "Coleman" and "MillCk_" by themselves.
 
   coleman <- subset(samdata, PopGroup == "Coleman") #Subset Coleman fish from all years.
@@ -75,17 +75,45 @@ lt.wt.mod <- nls(Weight_ ~ a*Length^b, data = bigfish, start = list(a=0.005, b=2
 summary(lt.wt.mod) #fitted function is highly significant
 coef(lt.wt.mod) # a=0.00001943926, b=2.89226591734 fitted parameter values
 
+####
+# Try nls model with form W=a*L^b but with weighted analysis because my data is has heteroscedasticity (variation in Weights changes across Lengths)
 
+# make dataset of only response and predictor variables (Length, Weight_)
+mod.dat <- bigfish[, c("Weight_", "Length")]
+
+# make column for values for weighting
+mod.dat$w <- 1/mod.dat$Weight_  # try 1/y for now, just to see if it makes a difference in tolerance intervals later.
+
+mod.dat$resid <- residuals(lt.wt.mod) # get residuals from nls object...could these residuals be used as weights??
+mod.dat$resid <- abs(mod.dat$resid)  #weights can't be negative, so take absolute value.
+attr(mod.dat$resid, "Residuals") <- NULL # Why isn't this working???
+
+
+# How to weight values? Can't calculate variance by sub-group (e.g., treatment) because all continuous variables.
+  # Here (https://www.r-bloggers.com/2012/07/a-weighting-function-for-nls-nlslm/) gives common examples like
+    # Weighting by inverse of response 1/y or Weighting by square root of predictor \sqrt{x}, etc...how how do I choose among these?  
+      # it also gives R code for a function wfct() that returns a vector of weights that are calculated from a user-defined expression and transfers this vector within nls.
+
+
+# Try nls model with weights
+lt.wt.modw <- nls(Weight_ ~ a*Length^b, data = mod.dat, start = list(a=0.005, b=2), weights = mod.dat$resid)
+summary(lt.wt.modw) #fitted function is highly significant
+coef(lt.wt.modw) # 0.00005562708, b=2.69175758515 slightly different fitted parameter values
+
+####
 
 # Plot weight by length with fitted equation
 ggplot(data=bigfish, aes(x=Length, y=Weight_, color=StudyID)) + theme_classic() + 
   geom_point() + theme(legend.title = element_blank(), legend.position = "bottom") +
-  stat_function(fun= function(x) 0.00001943926*x^2.89226591734, color="black", size = 1)
+  stat_function(fun= function(x) 0.00001943926*x^2.89226591734, color="black", size = 1) +
+  stat_function(fun= function(x) 0.00005562708*x^2.69175758515, color="blue", size = 1)
   # can tell that once I figure out how to estimate the tolerance interval, the interval
     # should increase with length (aka bigger salmon have a bigger gas tank)
   # I also think it is okay that late fall are on the bigger side (they are hatchery fish), while
     # steelhead are less heavy for their weight because they are wild. Maybe together they are a
     # good estimate of the real range of possibilities.
+
+    # blue line is weighted nls, black line is non-weighted nls
 
 
 # STEP 4: Use function to estimate prediction intervals (not confidence intervals) to capture the range
@@ -111,17 +139,13 @@ colnames(xy.data) <- c("y", "x")
 out2 <- nlregtol.int(formula, xy.data = xy.data, x.new=cbind(c(100, 150)), 
                      side = 2, alpha = 0.05, P = 0.95, start = list(a=0.00002, b=3))
 
+#try with weights
+out2w <- nlregtol.int(formula, xy.data = xy.data, x.new=cbind(c(100, 150)), 
+                     side = 2, alpha = 0.05, P = 0.95, start = list(a=0.00002, b=3), weights=mod.dat$resid)
+# ERROR trying to pass weights to nls inside nlregtol.int...
+
+
 head(out2)
-# ISSUES: Error if don't use x.new argument (Error in P.mat[i, ] : subscript out of bounds)
-        #If use x.new = cbind(c(10, 20)), Why are tolerance bounds so huge?! Not right.
-          # also do not understand what x.new is doing.
-          # lots of warnings: not converged in 10000 iter. - maybe this is why I'm getting weird estimates!
-            # Yes, directions say "It is highly recommended that the user specify starting values for the nls routine."
-            # BUT, I do not know how/where to specify the starting values in nlregtol.int -- OKAY did this, but still same issue and warnings :()
-
-plottol(out2, x = xy.data$x, y = xy.data$y, side = "two", x.lab = "X", y.lab = "Y")
-#ISSUES: can't get this plot to make sense
-
 #nlregtol.int returns a data frame with items:
 #  alpha The specified significance level.
 #P The proportion of the population covered by the tolerance bound(s).
@@ -132,9 +156,21 @@ plottol(out2, x = xy.data$x, y = xy.data$y, side = "two", x.lab = "X", y.lab = "
 #1-sided.upper The 1-sided upper tolerance bound. This is given only if side = 1.
 #2-sided.lower The 2-sided lower tolerance bound. This is given only if side = 2.
 #2-sided.upper The 2-sided upper tolerance bound. This is given only if side = 2.
+      
+plottol(out2, x = xy.data$x, y = xy.data$y, side = "two", x.lab = "Length", y.lab = "Weight", col="lightskyblue")
+# Looks good! However, tolerance intervals are even across salmon Length even though it should increase.
+  # I think this is because it uses an ordinary least squares analysis and I really want
+  # the tolerance intervals derived from an estimated weighted least squares analysis -
+  # I got this from this paper (https://www.hindawi.com/journals/jqre/2009/126283/#sec1.2)
+  # See Figure 1 and the description below it. They plot both types of tolerance intervals
+  # and it is obvious that one is even and the other changes width.
+  # HOWEVER, I can't figure out how to do a weighted analysis in the R tolerance package...
+  # OOO, nls has an option to add weights=... ! Explore what this is. Can pass nls stuff into the tolerance functions.
+    # go up and try above with nls! (https://www.r-bloggers.com/2014/01/ill-take-my-nls-with-weights-please/)
 
-
-
+# Make tolerance dataset
+tol.dat <- out2
+colnames(tol.dat) <- c("alpha", "P", "pred.Wt", "obs.Wt", "lower.tol", "upper.tol")
 
 ### When I tried STEP4 with prediction intervals...
 
@@ -164,18 +200,45 @@ ggplot() + theme_classic() +
   geom_ribbon(data=pred.dat, aes(x=Length, ymin=lwr, ymax=upr), alpha=0.1, fill="blue")
 
 
-# STEP 5: Plot the minimum prediction interval values (Xcrit) by the maximum prediction interval values (Xmax)
-ggplot(data=pred.dat, aes(x=upr, y=lwr)) + theme_classic() + 
-  geom_line(size=1) + xlab("upr.pred aka individual salmon prior Xmax") +
-  ylab("lwr.pred aka Xcrit")
-  # Cool! With the right prediction intervals, this is the shape/function for Xcrit (lwr.pred) in my model.
-  # But this is linear??? Should not be...?
+# STEP 5: Plot the minimum tolerance interval values (Xcrit) by the maximum tolerance interval values (Xmax)
+ggplot(data=tol.dat, aes(x=upper.tol, y=lower.tol)) + theme_classic() + 
+  geom_point(size=1.6) + xlab("upr.pred aka individual salmon prior Xmax") +
+  ylab("lwr.pred aka Xcrit") +
+  stat_function(fun= function(x) 0.9986592*x - 17.6462223, color="blue", size = 1)
+  # Cool! But I think I need to figure out how to make weighted tolerance intervals
 
 
 # STEP 6: Fit function to this relationship - use this function as Equation 3. Aka how once a salmon reaches an
     # Xmax value this will determine its Xcrit value in the model.
 
-    #Based on plot in Step 5: pick formula for this equation: 
+    #Based on plot in Step 5: pick formula for this equation: linear, y = mx+b
+
+summary(lm(lower.tol ~ upper.tol, tol.dat))
+# intercept = -17.6462223, slope = 0.9986592
+# BUT this will run into trouble for small salmon. and it is not biologically right. Sigh, will need to figure out the weighted nls/tolerance intervals.
+
+
+#####################################################################################
+## EQUATION 4: ENERGY GAINED IN THE RIVER IS A FUNCTION OF CURRENT BODY SIZE
+
+simfish4 <- seq(15, 180, by=2)
+ehab_per <- seq(0, .08, by=0.02)
+
+Ehab_g <- simfish4*0.08 # 8%
+
+plot(simfish4, Ehab_g)
+
+# Can a 180 g salmon gain 14 grams per day??? NO.
+    # (MacFarlane 2020) salmon max gain in the ocean in the summer is 1.5 g/day
+      # so function asymptotes to 1.5 g/day
+
+E.size <- function(X, a, b){  a-b/X }
+curve(E.size(X, a=1.5, b=12), xlim=c(0, 150), ylim=c(0, 1.8), ylab="E (g/day)",
+      xlab="Body weight (g)", main="Body weight influences Daily energy gained",
+      xname = "X")
+abline(h=1.5, col = "mediumslateblue", lty="dashed")
+# a defines maximum value (1.5 g/d)
+
 
 
 
